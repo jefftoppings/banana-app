5bd31e986837382d17aa0865ef8031565c392ef1
Jeff Toppings <jeffrey.toppings@outlook.com>
2020-02-14 20:13:27 -0600

    Create simple view of banana

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello world'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black12,
+        appBar: AppBar(
+          title: Text(
+            'Banana App',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.yellow[600],
+        ),
+        body: Center(
+          child: RotatedBox(
+            quarterTurns: 3,
+            child: Image(
+              image: AssetImage('images/banana.png'),
+            ),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
